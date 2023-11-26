e10c21996df844aeb7ce25c1754da3a41f09eba1
Rajesh Chaudhary <114743505+rajesh7688@users.noreply.github.com>
2023-11-26 22:52:19 +0530

    visual animation done

--- script/script.js
+++ script/script.js
@@ -28,38 +28,60 @@ function revealToSpan() {
 }
 revealToSpan();
 
-tl.from(".child span", {
-  x: 100,
-  duration: 1,
-  stagger: 0.2,
-  ease: Power3.easeInOut,
-})
-  .to(".parent .child", {
-    y: "-100%",
+const loaderAnimation = () => {
+  tl.from(".child span", {
+    x: 100,
     duration: 1,
-    ease: Circ.easeInOut,
+    stagger: 0.2,
+    ease: Power3.easeInOut,
   })
-  .to("#loader", {
-    height: 0,
-    duration: 1.5,
-    ease: Circ.easeInOut,
-  })
-  .to("#green", {
-    height: "100%",
-    top: 0,
-    duration: 1.3,
-    delay: -2,
-    ease: Circ.easeInOut,
-  })
-  .to("#green", {
-    height: "0%",
-    duration: 1,
-    delay: -0.6,
-    ease: Circ.easeInOut,
-  })
-  .to("#home", {
-    top: "0%",
-    duration: 2,
-    delay: -2.3,
-    ease: Circ.easeInOut,
+    .to(".parent .child", {
+      y: "-100%",
+      duration: 1,
+      ease: Circ.easeInOut,
+    })
+    .to("#loader", {
+      height: 0,
+      duration: 1.5,
+      ease: Circ.easeInOut,
+    })
+    .to("#green", {
+      height: "100%",
+      top: 0,
+      duration: 1.3,
+      delay: -2,
+      ease: Circ.easeInOut,
+    })
+    .to("#green", {
+      height: "0%",
+      duration: 1,
+      delay: -0.6,
+      ease: Circ.easeInOut,
+    })
+    .to("#home", {
+      top: "0%",
+      duration: 2,
+      delay: -2.3,
+      ease: Circ.easeInOut,
+    });
+};
+
+const visualAnimate = () => {
+  const svg = document.querySelectorAll("#Visual>g");
+  svg.forEach((e) => {
+    let path = e.childNodes[1].childNodes[1];
+    console.log(path.style);
+    path.style.strokeDasharray = path.getTotalLength() + "px";
+    path.style.strokeDashoffset = path.getTotalLength() + "px";
   });
+
+  gsap.to("#Visual>g>g>path, #Visual>g>g>polyline", {
+    strokeDashoffset: 0,
+    duration: 2,
+    delay: 5,
+    ease: Power3,
+  });
+};
+
+loaderAnimation();
+visualAnimate();
